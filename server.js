22c7361291363ca0e30ab01d7e2bed6849561b35
Ilkarion <Myfirststepsprogramming@gmail.com>
2025-09-27 18:16:38 +0300

    added deploied domain of frontend to cors

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: ["http://localhost:3000", "https://your-book-plen.vercel.app"], credentials: true }));
 
 
 app.use(express.json());
