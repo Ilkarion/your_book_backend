7da447cc332a6d08e8f40a9c0ebb52e9c124b70f
Ilkarion <Myfirststepsprogramming@gmail.com>
2026-02-07 21:13:45 +0200

    bugs001

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 
 // ===== MIDDLEWARE =====
 app.use(cors({
-  origin: ["http://localhost:3000", "https://diary-cosmic-q46z.vercel.app/"],
+  origin: ["http://localhost:3000", "https://diary-cosmic-q46z.vercel.app"],
   credentials: true
 }));
 app.use(express.json());
